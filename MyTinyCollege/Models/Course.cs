f25ce6b34ab81676b6fe34ed09ac88018018795b
Terrance <clarktk@hotmail.com>
2016-04-21 14:30:44 -0300

    Added our database model classes

---
+++ MyTinyCollege/Models/Course.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace MyTinyCollege.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public int DepartmentID { get; set; }//FK to Department
+
+        //============================Navigation Properties============================
+
+        //1 course to many enrollments
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+        //1 course to many instructors
+        public virtual ICollection<Instructor> Instructors { get; set; }
+
+        //1 course to 1 department
+        public virtual Department Department { get; set; }
+
+        //Combine CourseID and Title in one property
+        public string CourseIdTitle
+        {
+            get
+            {
+                return CourseID + ": " + Title;
+            }            
+        }
+
+    }
+}
